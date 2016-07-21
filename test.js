3e7fd6e5fd23a7a2244f06d069a288bfba69e595
jess-of-the-woods <what.the.dickenss@gmail.com>
2016-07-21 12:56:52 +1200

    subtract method, test subtract

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var test = require('tape').test;
 var add = require('./add');
+var subtract = require('./subtract')
 
 test('The add method', function(t){
   var actual = add(1,2);
@@ -7,3 +8,10 @@ test('The add method', function(t){
   t.equal(actual, expected, 'should add two numbers correctly.');
   t.end();
 });
+
+test('The subtract method', function(t){
+  var actual = subtract(10,2);
+  var expected = 8;
+  t.equal(actual, expected, 'should subtract one number from another.');
+  t.end();
+})
